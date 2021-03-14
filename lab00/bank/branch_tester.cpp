7deeb59958b283958f6144bfac3839752e0ce1df
PM Jat <pmjatz@gmail.com>
2021-03-14 15:41:28 +0530

    updates
    updates

--- lab00/bank/branch_tester.cpp
+++ lab00/bank/branch_tester.cpp
@@ -7,18 +7,26 @@ extern short NACC;
 extern BankAccount* accounts[];
 
 int main(int argc, char **argv) {
-
 	
 	init_branch();
 
-	long accno1 = open_account("Sanjay", 10000);
-	long accno2 = open_account("Pankaj", 1000);
-	long accno3 = open_account("Arnav", 5000);
-	long accno4 = open_account("Sanjay", 3000);
+	open_account("Sanjay", 10000);
+	open_account("Pankaj", 1000);
+	open_account("Arnav", 5000);
+	open_account("Sanjay", 3000);
+
+
+	BankAccount* acc;
+	//List All accounts
+	for (int i=0; i < NACC; i++ ) {
+		acc = accounts[i];
+		if (acc != NULL)
+			printf("Account: %s\n", ba_string(acc).c_str());		
+	} 
 
 	//Now suppose you want to depsoit 3000 to 
 	//   accno 1003.
-	BankAccount* acc = find_account( 1003 );
+	acc = find_account( 1003 );
 	deposit(acc, 3000);
 	//Show new balance of the account
 	printf("Account: %s\n", ba_string(acc).c_str());
